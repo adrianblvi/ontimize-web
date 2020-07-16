0b9d68fa25bacb23f030024c79f7fd9ba050f740
adrianblvi <adrianblancovi@gmail.com>
2020-07-16 11:39:30 +0200

    Created new render

--- src/app/main/accounts/accounts-home/accounts-home.component.ts
+++ src/app/main/accounts/accounts-home/accounts-home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { intRateMonthlyFunction } from '../../../shared/shared.module';
 @Component({
   selector: 'app-accounts-home',
   templateUrl: './accounts-home.component.html',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountsHomeComponent implements OnInit {
 
+  public intRateMonthly = intRateMonthlyFunction;
   constructor() { }
 
   ngOnInit() {
--- src/app/main/accounts/accounts.module.ts
+++ src/app/main/accounts/accounts.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { OntimizeWebModule} from 'ontimize-web-ngx';
+import { OntimizeWebModule } from 'ontimize-web-ngx';
 
 import { AccountsRoutingModule } from './accounts-routing.module';
 import { AccountsHomeComponent } from './accounts-home/accounts-home.component';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     OntimizeWebModule,
+    SharedModule,
     AccountsRoutingModule
   ],
   declarations: [AccountsHomeComponent]
